d267edf0fb1d0d54df4971ecbf69f48ad528c5ba
JackCarterSmith <arechristopher1@gmail.com>
2017-12-30 14:15:12 +0100

    Correct minor glitch
    Fix incorrect syntax for typedef
    Move function from header to main

--- main.c
+++ main.c
@@ -19,8 +19,7 @@ int main()
     lg=fopen("historiqueActions.log", "a");
     if(lg==NULL) return -1;
 
-    my_app=(int*)malloc(sizeof(int)*taille);
-    //(int*) = operation de casting pas nécessaire pour du C 99//
+    my_app=malloc(sizeof(int)*taille);
     if(my_app==NULL){printf("Pb\n");return -1;}
 
     do {
@@ -105,4 +104,104 @@ int main()
     fclose(lg);
     free(my_app);
     return 0;
+}
+
+//prototypes temporaires et à améliorer.
+
+int CmptPcExp(STAT A[], int taille, int Exp)
+{
+    int i=0;
+
+    for (i=0; i<taille; i++)
+    {
+
+    }
+}
+
+int ajoutFichePC(FICHE *pp, STAT *A, int taille, int Exp)
+{
+    FILE*f=NULL;  //A voir le format de la DB, pour l'instant je procède comme si la DB était un .txt
+    char nomfic[32];
+    char O;
+    char N;
+    printf("Nom du fichier DB ?\n");
+    scanf("%s", nomfic);
+    f=fopen(nomfic, "a");
+
+    if(f==NULL) //fichier innexistant
+    {
+        printf("DB introuvable");
+
+        if(f==NULL) //Le prog n'a pas les droits en écriture
+            return -1;
+    }
+
+    printf("\nID du PC ?\n"); fprintf(f, "ID : %s", pp->Nom);
+    printf("Son CPU ?\n"); fprintf(f, "CPU : %s", pp->CPU); A->Materiel=1; if (pp->CPU==NULL) {A->Materiel=0;}
+    printf("HDD ?\n"); fprintf(f, "HDD : %s", pp->HDD); A->Materiel=1; if (pp->HDD==NULL) {A->Materiel=0;}
+    printf("Son OS ?\n"); fprintf(f, "OS : %s", pp->OS); A->Os=1; if (pp->OS==NULL) {A->Os=0;}
+
+    if (A->Materiel == 1 && A->Os == 1) //Si la partie materielle et l'os présentes, l'etat est O pour oui.
+    {
+        fprintf(f, "Etat : O\n"); Exp=Exp+1;
+    }
+
+    else
+    {
+        fprintf(f, "Etat : N\n"); //Dans le cas contraire, l'etat est N pour non
+    }
+
+    printf("Drivers installés ?\n"); fprintf(f, "Drivers : %s", pp->Drivers);  //Drivers présents mais optionnels
+    printf("Divers software ?\n"); fprintf(f, "Softwares : %s", pp->Softwares); //Logiciels présents mais optionnels
+
+    taille = taille+1;
+    fclose(f);
+    return taille;
+}
+
+int supprimePC(FICHE *F, char *ID)
+{
+	FILE*f=NULL;
+	int pos=0;
+	int trouve=0;
+	char nomfic[32];
+
+	printf("Nom du fichier DB ?\n");
+	scanf("%s", nomfic); //exemple "DB.db"
+	f=fopen(nomfic, "r+"); //En revanche j'ai écrit cette fonction de la même manière que pour un .txt donc méthode à vérifier.
+
+	if(f==NULL) //Si le fichier n'existe pas, on retourne une erreur
+	{
+		return 0;
+	}
+
+	rewind(f); //On se place au début du fichier
+
+	while(fscanf(f,"%s\n",F->Nom)!=EOF)
+	{
+		if(strcmp(F->Nom, ID)==0)
+		{
+			trouve=1;
+			break;
+		}
+	pos++;
+	}
+
+	if (trouve == 0) return 0;
+
+	printf("fiche trouvée\n");
+
+	while(fscanf(f,"%s\n",F->Nom)!=EOF)
+	{
+		fseek(f,pos*52,SEEK_SET); //taille 52 arbitraire a vérifier la taille en octet d'une ligne sachant que la DB sera en binaire
+		fprintf(f,"%-20s%",F->Nom);
+		pos++;
+		fseek(f,(pos+1)*52,SEEK_SET);
+	}
+
+	fseek(f,pos*52,SEEK_SET);
+	fprintf(f,"%c",26);
+	fclose(f);
+
+	return 1;
 }
