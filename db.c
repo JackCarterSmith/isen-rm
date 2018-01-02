f8fa9dc0239de3b3a4742920610b2957729e279d
JackCarterSmith <arechristopher1@gmail.com>
2018-01-02 11:52:57 +0100

    Added createDB function

--- db.c
+++ db.c
@@ -4,13 +4,48 @@
  *  Created on: 2 janv. 2018
  *      Author: are
  *
- *      DB Core - v1
+ *      DB Core
  */
+#define VERSION 1.0
 #include <stdlib.h>
 #include <stdio.h>
 #include "db.h"
 
 int regenDBFile(){
+	/*
+	 * Template DB :
+	 * <HEAD>
+	 * 	conf_opt(value)
+	 * 	...
+	 * </HEAD>
+	 * <DATA>
+	 * 	<FICHEx>
+	 * 	</FICHEx>
+	 * </DATA>
+	 *
+	 * Contenu de l'en-tête :
+	 * Version de la DB (float)
+	 * Nombre d'entrées (int)
+	 * Compteur 1 (int)
+	 * Compteur 2 (int)
+	 */
+	FILE *db = NULL;
+	HEAD default_head_data;
+
+	db = fopen("db.irm","wb+");
+	if (db != NULL){
+		default_head_data.version_db = VERSION;
+		default_head_data.nbr_fiches = 0;
+		default_head_data.cpt1 = 0;
+		default_head_data.cpt2 = 0;
+		fwrite(&default_head_data, sizeof(HEAD), 1, db);
+
+		fclose(db);
+	} else {
+		return -1;		//Problème dans la lecture du fichier
+		//Ajouter d'une entrée dans le log !
+	}
+
 	return 0;
 }
 
--- db.h
+++ db.h
@@ -4,11 +4,18 @@
  *  Created on: 2 janv. 2018
  *      Author: are
  *
- *      DB Core - v1
+ *      DB Core
  */
 
 #ifndef DB_H_
 #define DB_H_
+
+typedef struct head_db{
+	float version_db;		//Pour la compatibilité et la migration des données
+	int nbr_fiches;
+	int cpt1;
+	int cpt2;
+}HEAD;
 
 int regenDBFile();  //Regénère la base de donnée, la remplace si elle existe déjà. Retourne 0 quand aucun problème.
 int backupDB();		//Créer une backup de la DB dans un sous dossier "backup"
